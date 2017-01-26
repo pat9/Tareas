728af970c21db550a9e6eacde03d4d1fdba5a803
Daniel Pat <danielpat6@gmail.com>
2017-01-25 19:00:27 -0600

    Practica empleado terminada

---
+++ Tareas/PracticaPersona/Empleado.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticaPersona
+{
+    class Empleado : Persona
+    {
+       protected double sueldo;
+
+        public double Sueldo
+        {
+            get
+            {
+                return sueldo;
+            }
+
+            set
+            {
+                sueldo = value;
+            }
+        }
+
+    }
+}
---
+++ Tareas/PracticaPersona/Persona.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticaPersona
+{
+    class Persona
+    {
+        protected string nombre;
+        protected int edad;
+
+        public string Nombre
+        {
+            get
+            {
+                return nombre;
+            }
+
+            set
+            {
+                nombre = value;
+            }
+        }
+
+        public int Edad
+        {
+            get
+            {
+                return edad;
+            }
+
+            set
+            {
+                edad = value;
+            }
+        }
+    }
+}
